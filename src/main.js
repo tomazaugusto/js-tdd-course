bff0de070e9875574ec15176bd4a88376b2b35d8
tomazaugusto <tomazaugustomsn@hotmail.com>
2019-02-25 13:28:50 -0300

    Testes

--- src/main.js
+++ src/main.js
@@ -3,5 +3,3 @@ const b = 2
 const b = 4
 
 const c = "abcd";
-
-console.log( "A  ")
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    filename: './app.js'
+  },
+  output: {
+    filename: './build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            ['es2015', { modules: false }]
+          ]
+        }
+      }
+    ]
+  }
+}
